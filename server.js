a7563dee945e7ca185f7d8b018aafa1673db811a
Nate-Asmare <natnaelasmare777@gmail.com>
2021-07-17 16:50:56 +0300

    removed comment from root server dir

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ const saltRounds = 10;
 
 const salt = bcrypt.genSaltSync(saltRounds);
 
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//         host: '127.0.0.1',
-//         user: 'postgres',
-//         password: 'leantan@7525',
-//         database: 'smartbrain'
-//     }
-// });
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'leantan@7525',
+        database: 'smartbrain'
+    }
+});
 
 // console.log(postgres.select('*').from('users'));
 const path = require('path');
@@ -40,9 +40,9 @@ app.use(cors())
 /image --> PUT --> user
 */
 
-// app.get('/', (req, res) => {
-//     res.send(database.users); //we can remove this code yes we should
-// })
+app.get('/', (req, res) => {
+    res.send("It's working!")
+})
 
 app.get('/profile/:id', (req, res) =>  profile.handleProfile(req, res, db))
 
