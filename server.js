b6767e3d9d8c1f08c6e2b9b39959b9ffe687b75d
Nate-Asmare <natnaelasmare777@gmail.com>
2021-07-17 18:39:52 +0300

    using db connection exactly as the heroku documentation

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const salt = bcrypt.genSaltSync(saltRounds);
 
 const db = knex({
     client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
 });
 
