6447c4d93094570e9724817c9333009716aece6d
Brad Traversy <natnaelasmare777@gmail.com>
2021-07-17 16:13:25 +0300

    commenting Mrkan code on server file

--- server.js
+++ server.js
@@ -53,14 +53,14 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 app.put('/image', (req, res) => {image.handleImage(req, res, db, bcrypt)})
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 app.listen(PORT, ()=> {
     console.log(`app is running on port ${PORT}`);
 })
